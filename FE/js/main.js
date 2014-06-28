96f05e96dd8c76c2fd5b87854b65afe11c9e00d0
sheak <mr.sheak@gmail.com>
2014-06-28 14:18:09 +0800

    调整公共Js
    头部交互公共js

--- FE/js/main.js
+++ FE/js/main.js
@@ -5,7 +5,22 @@
 
 ;$(function(){
 /*********************************************************/
-    
+    // 更多
+    $(".show_more").mouseenter( function(){
+        $(this).find(".more_item").stop(true , true).fadeIn();
+    });
+
+    $(".show_more").mouseleave(function(){
+        $(this).find(".more_item").stop(true , true).fadeOut();
+    });
+
+    // 首页消息中心
+    $(".msg_tab li").click(function(){
+        var index = $(this).index();
+        $(this).addClass("cur").siblings("li").removeClass("cur");
+
+        $(".msg_item").eq(index).show().siblings(".msg_item").hide();
+    });
 
     // 显示二维码
     $(".wx").hover(function(){
