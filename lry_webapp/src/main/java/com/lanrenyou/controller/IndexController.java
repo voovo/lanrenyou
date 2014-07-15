f6d52ef0ecc2cca059ceb74bd26ee217ed3af8a0
voovo <jsan163@163.com>
2014-07-15 19:04:38 +0800

    add index page

---
+++ lry_webapp/src/main/java/com/lanrenyou/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.lanrenyou.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.lanrenyou.controller.base.BaseController;
+
+@Controller
+@RequestMapping("")
+public class IndexController  extends BaseController {
+	
+	@RequestMapping("/index")
+	public ModelAndView toAddPage(){
+		return new ModelAndView("/index");
+	}
+
+}
