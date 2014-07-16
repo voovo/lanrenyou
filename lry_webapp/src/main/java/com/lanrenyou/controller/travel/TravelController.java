d39fcc258e9d11557c90a848834cdd14f165a887
voovo <jsan163@163.com>
2014-07-16 18:17:33 +0800

    modify create travel

--- lry_webapp/src/main/java/com/lanrenyou/controller/travel/TravelController.java
+++ lry_webapp/src/main/java/com/lanrenyou/controller/travel/TravelController.java
@@ -15,15 +15,12 @@ import org.springframework.web.servlet.ModelAndView;
 import com.lanrenyou.controller.base.BaseController;
 import com.lanrenyou.travel.service.ITravelContentService;
 import com.lanrenyou.travel.service.ITravelInfoService;
-import com.lanrenyou.travel.service.ITravelVisitLogService;
 import com.lanrenyou.travel.enums.TravelInfoIsEliteEnum;
 import com.lanrenyou.travel.enums.TravelInfoIsTopEnum;
 import com.lanrenyou.travel.enums.TravelInfoStatusEnum;
 import com.lanrenyou.travel.model.TravelContent;
 import com.lanrenyou.travel.model.TravelInfo;
-import com.lanrenyou.travel.model.TravelVisitLog;
 import com.lanrenyou.user.model.UserInfo;
-import com.lanrenyou.util.ServletUtil;
 
 @Controller
 @RequestMapping("/travel")
@@ -68,15 +65,25 @@ public class TravelController  extends BaseController {
 		travelInfo.setCreateUid(userInfo.getId());
 		travelInfo.setCreateTime(new Date());
 		travelInfo.setCreateIp(this.getRemoteAddr());
-		travelInfoService.addTravelInfo(travelInfo);
+		int result = travelInfoService.addTravelInfo(travelInfo);
+		if(result < 1){
+			map.put("status", "n");
+			map.put("info", "插入数据失败");
+			return gson.toJson(map);
+		}
 		
 		TravelContent travelContent = new TravelContent();
 		travelContent.setTid(travelInfo.getId());
+		travelContent.setContent(imgs);
+		result = travelContentService.addTravelContent(travelContent);
+		if(result < 1){
+			map.put("status", "n");
+			map.put("info", "插入游记内容失败，请稍后重试");
+			return gson.toJson(map);
+		}
 		
-		
-		
-		map.put("status", "n");
-		map.put("info", "系统正忙，请稍后再试");
+		map.put("status", "y");
+		map.put("info", "创建成功");
 		return gson.toJson(map);
 	}
 }
