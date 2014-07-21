b957d4cbb0a34083c81b251b1b54e4bb86651ac4
voovo <jsan163@163.com>
2014-07-21 14:14:53 +0800

    deal solr time type

--- lry_webapp/src/main/java/com/lanrenyou/search/index/util/DateUtil.java
+++ lry_webapp/src/main/java/com/lanrenyou/search/index/util/DateUtil.java
@@ -6,8 +6,15 @@ import java.sql.Timestamp;
 import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
+
+import org.apache.commons.lang.StringUtils;
 
 public class DateUtil {
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	private static SimpleDateFormat sdf2 = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy",Locale.US);
+	
 	/*
 	 * java编程
 	 */
@@ -132,10 +139,6 @@ public class DateUtil {
 		else
 			return date.substring(11, 19);
 	}
-
-	public static void main(String args[]) {
-		System.out.println(now());
-	}
 	
 	public static String isNullShow(String str){
 		if(str==null){
@@ -158,4 +161,108 @@ public class DateUtil {
 		}
 		
 	}
+		
+	/**
+	 * 本地时间转为UTC时间（UTC时间比北京时间早8个小时）
+	 * @param date
+	 * @return
+	 */
+	public static Date converUTCDate(Date date){
+		if(date==null){
+			return null;
+		}
+		//1、取得本地时间：  
+		java.util.Calendar cal = java.util.Calendar.getInstance();  
+		try{
+			cal.setTime(date);
+			//2、取得时间偏移量：  
+			int zoneOffset = cal.get(java.util.Calendar.ZONE_OFFSET);  
+			//3、取得夏令时差：  
+			int dstOffset = cal.get(java.util.Calendar.DST_OFFSET);  
+			//4、从本地时间里扣除这些差量，即可以取得UTC时间：  
+			cal.add(java.util.Calendar.MILLISECOND, -(zoneOffset + dstOffset));  
+		}catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
+		return (new Date(cal.getTimeInMillis()));
+	}
+	
+	/**
+	 * UTC时间转为当地时间（UTC时间比北京时间早8个小时）
+	 * @param date
+	 * @return
+	 */
+	public static Date converBJDate(Date date){
+		if(date==null){
+			return null;
+		}
+		//1、取得本地时间：  
+		java.util.Calendar cal = java.util.Calendar.getInstance();  
+		try{
+			cal.setTime(date);
+			//2、取得时间偏移量：  
+			int zoneOffset = cal.get(java.util.Calendar.ZONE_OFFSET);  
+			//3、取得夏令时差：  
+			int dstOffset = cal.get(java.util.Calendar.DST_OFFSET);  
+			//4、UTC时间加上这些差量，即可得到当地时间：  
+			cal.add(java.util.Calendar.MILLISECOND, +(zoneOffset + dstOffset));  
+		}catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
+		return (new Date(cal.getTimeInMillis()));
+	}
+
+	/**
+	 * 加8小时
+	 * @param utcStr
+	 * @return
+	 */
+	public static synchronized String converUTCStr2BJStr(String utcStr){
+		if(!StringUtils.isNotBlank(utcStr)){
+			return "";
+		}else{
+			try {
+				return sdf.format(converBJDate(sdf2.parse(utcStr)));
+			} catch (Exception e) {
+				e.printStackTrace();
+				return "";
+			}
+		}
+		
+	}
+	/**
+	 * 减8小时
+	 * @param bjStr
+	 * @return
+	 */
+	public static synchronized String converBJStr2UTCStr(String bjStr){
+		if(!StringUtils.isNotBlank(bjStr.trim())){
+			return "";
+		}else{
+			try {
+				return sdf.format(converUTCDate(sdf2.parse(bjStr)));
+			} catch (Exception e) {
+				e.printStackTrace();
+				return "";
+			}
+		}
+		
+	}
+	
+	public static void main(String[] args) throws Exception{
+
+//			System.out.println(DateUtils.converUTCDate(sdf2.parse("Mon Aug 09 08:00:00 CST 1993")));
+//			System.out.println(DateUtils.converBJDate(sdf2.parse("Mon Aug 09 08:00:00 CST 1993")));
+//			System.out.println(DateUtils.converUTCStr2BJStr("Mon Aug 09 08:00:00 CST 1993"));
+		System.out.println(DateUtil.converBJStr2UTCStr("Tue Aug 02 06:58:30 CST 2011"));
+		
+//			System.out.println(DateUtils.converUTCDate(new Date("Mon Aug 09 08:00:00 CST 1993")));
+//			System.out.println(DateUtils.converBJDate(new Date("Mon Aug 09 08:00:00 CST 1993")));
+		//		System.out.println(DateUtils.converBJDate(new Date()));
+//			System.out.println(DateUtils.converBJDate(sdf.parse("2011-08-15 04:28:55")));
+	}
+
+
 }
--- lry_webapp/src/main/java/com/lanrenyou/search/index/util/SolrUtil.java
+++ lry_webapp/src/main/java/com/lanrenyou/search/index/util/SolrUtil.java
@@ -1,22 +1,19 @@
 package com.lanrenyou.search.index.util;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 
 import mybatis.framework.core.support.PageIterator;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.solr.client.solrj.SolrQuery;
-import org.apache.solr.client.solrj.SolrRequest.METHOD;
+import org.apache.solr.client.solrj.SolrQuery.ORDER;
 import org.apache.solr.client.solrj.SolrServer;
 import org.apache.solr.client.solrj.SolrServerException;
-import org.apache.solr.client.solrj.SolrQuery.ORDER;
 import org.apache.solr.client.solrj.impl.CommonsHttpSolrServer;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocument;
@@ -24,7 +21,6 @@ import org.apache.solr.common.SolrDocumentList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.gson.Gson;
 import com.lanrenyou.travel.model.TravelInfo;
 
 public class SolrUtil {
@@ -33,12 +29,10 @@ public class SolrUtil {
 	
 	private String lryPlannerServer;
 	
-	private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+	private static SimpleDateFormat sdf2 = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy",Locale.US);
 	
 	protected Logger logger = LoggerFactory.getLogger(getClass());
 	
-	private static final Gson gson = new Gson();
-
 	public String getLryTravelServer() {
 		return lryTravelServer;
 	}
@@ -128,8 +122,8 @@ public class SolrUtil {
 		
 		try {
 			QueryResponse resp = this.getLryTravelServers()[0].query(query);
-			totalCount = (Integer) resp.getResponse().size();	// 命中个数怎么取？
 			SolrDocumentList docList = resp.getResults();
+			totalCount = (int) docList.getNumFound();	// 命中个数
 			Iterator<SolrDocument> docIter = docList.iterator();
 			travelInfoList = new ArrayList<TravelInfo>();
 			while(docIter.hasNext()){
@@ -137,17 +131,19 @@ public class SolrUtil {
 				Integer tid = (Integer) doc.getFieldValue("tid");
 				String title = (String) doc.getFieldValue("titile");
 				Integer uid = (Integer) doc.getFieldValue("uid");
-				Date createTime = (Date) doc.getFieldValue("create_time");	// 时间类型怎么处理？
+				String createTimeS = doc.getFieldValue("createTime").toString();
 				String content = (String) doc.getFieldValue("content");
 				TravelInfo travelInfo = new TravelInfo();
 				travelInfo.setId(tid);
 				travelInfo.setTitle(title);
 				travelInfo.setUid(uid);
-				travelInfo.setCreateTime(createTime);
+				travelInfo.setCreateTime(DateUtil.converUTCDate(sdf2.parse(createTimeS)));
 				travelInfo.setContent(content);
 				travelInfoList.add(travelInfo);
 			}
 		} catch (SolrServerException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
 			e.printStackTrace();
 		}
 		PageIterator<TravelInfo> pageIterator = PageIterator.createInstance(pageNo, pageSize, totalCount);
@@ -155,56 +151,4 @@ public class SolrUtil {
 		return pageIterator;
 	}
 	
-	public static void main(String[] args) throws MalformedURLException{
-		Gson gson = new Gson();
-		SolrQuery query = new SolrQuery();
-		query.setStart(0);
-		query.setRows(10);
-		String keyword = "asas";
-		String city = null;
-		StringBuilder querySb = new StringBuilder();
-		if(StringUtils.isNotBlank(city)){
-			querySb.append("city:").append(city);
-		}
-		if(StringUtils.isNotBlank(keyword)){
-			StringBuilder keywordSb = new StringBuilder("(");
-			keywordSb.append("title:").append(keyword).append(" or ");
-			keywordSb.append("city:").append(keyword).append(" or ");
-			keywordSb.append("content:").append(keyword).append(" or ");
-			keywordSb.append("user_name:").append(keyword).append(" or ");
-			keywordSb.append("user_nickname:").append(keyword).append(" or ");
-			keywordSb.append("planner_targetCity:").append(keyword).append(" or ");
-			keywordSb.append("user_userIntro:").append(keyword);
-			keywordSb.append(")");
-			
-			if(querySb.length() > 0){
-				querySb.append(" and ").append(keywordSb);
-			} else {
-				querySb.append(keywordSb.substring(1, keywordSb.length() -1 ));
-			}
-		}
-		
-		query.setQuery(querySb.toString());
-		
-		query.setSortField("updateTime", ORDER.desc);
-		
-		System.out.println(gson.toJson(query));
-		try {
-			CommonsHttpSolrServer server = new CommonsHttpSolrServer("http://search.lanrenyou.com/travel/");
-			server.setConnectionTimeout(10000); // 1/10th sec
-			server.setDefaultMaxConnectionsPerHost(100);
-			server.setMaxTotalConnections(100);
-			QueryResponse resp = server.query(query, METHOD.POST);
-			SolrDocumentList docList = resp.getResults();
-			Iterator<SolrDocument> docIter = docList.iterator();
-			while(docIter.hasNext()){
-				System.out.println(gson.toJson(docIter.next()));
-			}
-		} catch (SolrServerException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
 }
