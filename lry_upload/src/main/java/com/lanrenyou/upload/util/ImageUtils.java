e75b8fde28266edc77c04a95a30d37ddf8b7c89a
voovo <jsan163@163.com>
2014-07-28 10:34:38 +0800

    debug image upload

--- lry_upload/src/main/java/com/lanrenyou/upload/util/ImageUtils.java
+++ lry_upload/src/main/java/com/lanrenyou/upload/util/ImageUtils.java
@@ -3,6 +3,8 @@ package com.lanrenyou.upload.util;
 import java.awt.Image;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -73,6 +75,10 @@ public class ImageUtils {
 			cropImage(operateImgPath, toPath285x390, x, y, toWidth, toHeight, srcPath.substring(srcPath.lastIndexOf('.') + 1), "jpeg");
 		} else {
 			// 不做处理
+			String dJPGPath = srcPath.substring(0, srcPath.lastIndexOf('.')) + "_d.jpg";
+			String sJPGPath = srcPath.substring(0, srcPath.lastIndexOf('.')) + "_s.jpg";
+			copyFile(new File(srcPath), new File(dJPGPath));
+			copyFile(new File(srcPath), new File(sJPGPath));
 		}
 	}
 	
@@ -206,5 +212,33 @@ public class ImageUtils {
 			}
 		}
 	}
+	
+	// 复制文件
+    public static void copyFile(File sourceFile, File targetFile) throws IOException {
+        BufferedInputStream inBuff = null;
+        BufferedOutputStream outBuff = null;
+        try {
+            // 新建文件输入流并对它进行缓冲
+            inBuff = new BufferedInputStream(new FileInputStream(sourceFile));
+
+            // 新建文件输出流并对它进行缓冲
+            outBuff = new BufferedOutputStream(new FileOutputStream(targetFile));
+
+            // 缓冲数组
+            byte[] b = new byte[1024 * 5];
+            int len;
+            while ((len = inBuff.read(b)) != -1) {
+                outBuff.write(b, 0, len);
+            }
+            // 刷新此缓冲的输出流
+            outBuff.flush();
+        } finally {
+            // 关闭流
+            if (inBuff != null)
+                inBuff.close();
+            if (outBuff != null)
+                outBuff.close();
+        }
+    }
 
 }
